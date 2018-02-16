5cda3c94e7bfd0024a9a04d5537413ba79352acd
mfouda214 <m.fouda@robone.net>
2018-02-16 22:18:05 +0000

    initial commit

---
+++ TabBarDemo/CalenderViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CalenderViewController.swift
+//  TabBarDemo
+//
+//  Created by Mohamed Sobhi  Fouda on 2/16/18.
+//  Copyright © 2018 Mohamed Sobhi Fouda. All rights reserved.
+//
+
+import UIKit
+
+class CalenderViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
---
+++ TabBarDemo/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  TabBarDemo
+//
+//  Created by Mohamed Sobhi  Fouda on 2/16/18.
+//  Copyright © 2018 Mohamed Sobhi Fouda. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func jumpToCalendarTab(_ sender: UIButton) {
+    
+        tabBarController?.selectedIndex = 1
+        
+    }
+    
+}
+
